0bd083188e21d34c6e247a1c24469b31622ab475
kinyuykelly <kinyuykelly213@gmail.com>
2023-05-23 14:39:29 +0100

    style:added roouters to components

--- src/app/module/kc-auth/kc-auth-routing.module.ts
+++ src/app/module/kc-auth/kc-auth-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SigninComponent } from './components/signin/signin.component';
+import { SignupComponent } from './components/signup/signup.component';
 import { KcAuthComponent } from './kc-auth.component';
 
-const routes: Routes = [{ path: '', component: KcAuthComponent }];
+const routes: Routes = [{ path: '', component: KcAuthComponent, 
+  children: [
+    {path: 'signin', component: SigninComponent},
+    {path: 'signup', component: SignupComponent}
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
