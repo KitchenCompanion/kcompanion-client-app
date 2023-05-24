f50f7ce8866f1a8ba971d92c152643d1d0c0921b
kinyuykelly <kinyuykelly213@gmail.com>
2023-05-24 23:24:56 +0100

    feat:Modified recipe details

--- src/app/module/kc-user/kc-user-routing.module.ts
+++ src/app/module/kc-user/kc-user-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipeDetailsComponent} from './components/recipe-details/recipe-details.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
+
+
 import { KcUserComponent } from './kc-user.component';
 
-const routes: Routes = [{ path: '', component: KcUserComponent }];
+const routes: Routes = [
+  { path: '', component: KcUserComponent,
+children:[
+  {path: "", component: RecipeComponent},
+  {path: "recipe", component: RecipeComponent},
+  {path: 'recipe-details', component: RecipeDetailsComponent}
+]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
