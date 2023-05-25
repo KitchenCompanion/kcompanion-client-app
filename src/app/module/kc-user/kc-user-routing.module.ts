b7dc5ea9e320db2f916066b0d597edfe9c6bb277
Favour Kimbi <64668403+kimbifavour@users.noreply.github.com>
2023-05-25 04:41:10 +0100

    Revert "Added categories,create-recipe and recipe-details components"

--- src/app/module/kc-user/kc-user-routing.module.ts
+++ src/app/module/kc-user/kc-user-routing.module.ts
@@ -1,19 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RecipeDetailsComponent} from './components/recipe-details/recipe-details.component';
-import { RecipeComponent } from './components/recipe/recipe.component';
-
-
 import { KcUserComponent } from './kc-user.component';
 
-const routes: Routes = [
-  { path: '', component: KcUserComponent,
-children:[
-  {path: "", component: RecipeComponent},
-  {path: "recipe", component: RecipeComponent},
-  {path: 'recipe-details', component: RecipeDetailsComponent}
-]
-}];
+const routes: Routes = [{ path: '', component: KcUserComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/module/kc-user/kc-user.module.ts
+++ src/app/module/kc-user/kc-user.module.ts
@@ -5,18 +5,12 @@ import { KcUserRoutingModule } from './kc-user-routing.module';
 import { KcUserComponent } from './kc-user.component';
 import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
 import { KcSharedModule } from '../kc-shared/kc-shared.module';
-import { CreateRecipeComponent } from './components/create-recipe/create-recipe.component';
-import { CategoriesComponent } from './components/categories/categories.component';
-import { RecipeComponent } from './components/recipe/recipe.component';
 
 
 @NgModule({
   declarations: [
     KcUserComponent,
-    RecipeDetailsComponent,
-    CreateRecipeComponent,
-    CategoriesComponent,
-    RecipeComponent
+    RecipeDetailsComponent
   ],
   imports: [
     CommonModule,
