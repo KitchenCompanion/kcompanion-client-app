de0113297e0f09002ad64e8849decb484a48c15e
kinyuykelly <kinyuykelly213@gmail.com>
2023-05-25 14:10:09 +0100

    chore:Restructured the project

--- src/app/module/kc-user/kc-user-routing.module.ts
+++ src/app/module/kc-user/kc-user-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { CreateRecipeComponent } from './components/create-recipe/create-recipe.component';
+import { RecipeDetailsComponent } from './components/recipe-details/recipe-details.component';
+import { RecipeComponent } from './components/recipe/recipe.component';
 import { KcUserComponent } from './kc-user.component';
 
-const routes: Routes = [{ path: '', component: KcUserComponent }];
+const routes: Routes = [{ path: '', component: KcUserComponent,
+children:[
+  {path: '', component: RecipeComponent},
+  {path: 'recipe', component: RecipeComponent},
+  {path: 'categories', component: CategoriesComponent},
+  {path: 'create-recipe', component: CreateRecipeComponent},
+  {path: 'recipe-details', component: RecipeDetailsComponent},
+
+
+] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
